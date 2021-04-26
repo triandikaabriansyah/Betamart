077990a0c27206737bdf81729fcfd716a17c6409
triandikaabriansyah <triandikaabriansyah@gmail.com>
2021-04-26 11:25:52 +0700

    first commit

---
+++ 25_Beta_Mart/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _25_Beta_Mart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel.Enabled = true;
+                txtName.Focus();
+                this.barangData.Barang.AddBarangRow(this.barangData.Barang.NewBarangRow());
+                barangBindingSource.MoveLast();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                barangBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = true;
+            txtName.Focus();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+            panel.Enabled = false;
+            barangBindingSource.ResetBindings(false);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                barangBindingSource.EndEdit();
+                barangTableAdapter.Update(this.barangData.Barang);
+                panel.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                barangBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true, Multiselect = false })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                        pictureBox.Image = Image.FromFile(ofd.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'barangData.Barang' table. You can move, or remove it, as needed.
+            this.barangTableAdapter.Fill(this.barangData.Barang);
+            barangBindingSource.DataSource = this.barangData.Barang;
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (MessageBox.Show("Apakah kamu yakin hapus barang ini ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    barangBindingSource.RemoveCurrent();
+            }
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (string.IsNullOrEmpty(txtSearch.Text))
+                {
+                    this.barangTableAdapter.Fill(this.barangData.Barang);
+                    barangBindingSource.DataSource = this.barangData.Barang;
+                    //dataGridView.DataSource = barangBindingSource;
+                }
+                else
+                {
+                    var query = from o in this.barangData.Barang
+                                where o.Namabarang.Contains(txtSearch.Text) || o.Stockbarang == txtSearch.Text || o.Hargabeli == txtSearch.Text || o.Hargajual.Contains(txtSearch.Text)
+                                select o;
+                    barangBindingSource.DataSource = query.ToList();
+                    //dataGridView.DataSource = query.ToList();
+                }                
+            }
+        }
+    }
+}
